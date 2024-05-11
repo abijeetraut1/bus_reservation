bf6b6097bfe111de85b25e1d8a1e2946ed078042
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2024-05-11 17:33:24 +0545

    added admin seeding

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 // const app = require("./app");
 const port = 8000 || process.env.PORT;
+const {
+    QueryTypes,
+    Sequelize
+} = require("sequelize");
 const server = require("./app");
 require("dotenv").config();
+const bcrypt = require("bcrypt");
+const database = require("./model/index");
+
+(async () => {
+
+    const phoneNo = process.env.phoneNo;
+    const name = process.env.admin_name;
+    const role = process.env.role;
+    const password = await bcrypt.hash(process.env.password, 12);
+
+    const user = await database.sequelize.query("SELECT * FROM users WHERE users.role = 'super-admin'", {
+        type: QueryTypes.SELECT
+    });
+    
+    if (user.length == 0) {
+
+        await database.sequelize.query(`INSERT INTO users (phoneNo, name, role, password) VALUES (?, ?, ?, ?)`, {
+            type: QueryTypes.INSERT,
+            replacements: [phoneNo,
+                name,
+                role,
+                password,
+            ]
+        }).then(() => {
+            console.log("admin seeded successfully");
+        }).catch((err) => {
+            console.log(err)
+            console.log("admin already seeded")
+        })
 
 
+    }
 
+})();
 
 server.listen(port, () => {
     console.log("server is running at port: " + port);
